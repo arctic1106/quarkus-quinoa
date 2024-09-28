abf60b1b194906f7cc580c8a42d65f782b739969
arctic1106 <arcticsoft@outlook.com>
2024-09-28 20:06:48 +0200

    init

---
+++ src/main/webui/counter.js
@@ -0,0 +1,9 @@
+export function setupCounter(element) {
+  element.innerHTML = `count is 0`
+
+  let counter = 0
+  element.addEventListener('click', () => {
+    counter++
+    element.innerHTML = `count is ${counter}`
+  })
+}
